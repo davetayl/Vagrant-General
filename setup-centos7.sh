c2b968759307b8298c75e0962a168b744c803bad
Dave <dave@imagetaylor.com>
2020-07-22 15:19:09 +1000

    Create setup-centos7.sh

---
+++ setup-centos7.sh
@@ -0,0 +1,36 @@
+# setup-centos7.sh
+
+# Configure script
+set -e # Stop script execution on any error
+echo ""; echo "---- Setting Up CentOS 8.x ----"
+
+# Configure variables
+TESTPOINT=google.com
+echo "- Variables Set -"
+
+# Test internet connectivity
+ping -q -c5 $TESTPOINT > /dev/null 2>&1
+ 
+if [ $? -eq 0 ]
+then
+	echo "- Internet Ok -"	
+else
+	echo "- Internet Failed! -"
+fi
+
+
+# Sync clock
+echo "- Sync Clock -"
+cat >> /etc/ntp.conf <<EOF
+server 0.au.pool.ntp.org
+EOF
+
+systemctl enable --now ntpd.service
+
+# Base OS update
+echo "- Update OS -"
+yum -yqe 3 update
+echo "- Install Tools -"
+yum -yqe 3 install net-tools bind-utils wget tree
+
+echo "---- CentOS Base Setup Complete ----"; echo ""
