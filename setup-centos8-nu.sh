2a1bb021246cb36043918e8006fb3e168c347bce
David Taylor <dave@imagetaylor.com>
2020-07-26 09:11:37 +1000

    added no update version of CentOS 8

---
+++ setup-centos8-nu.sh
@@ -0,0 +1,33 @@
+# setup-centos8.sh
+
+# Configure script
+set -e # Stop script execution on any error
+echo ""; echo "---- Setting Up CentOS 8.x (No update)----"
+
+# Configure variables
+TESTPOINT=google.com
+echo "- Variables Set -"
+
+# Test internet connectivity
+ping -q -c5 $TESTPOINT > /dev/null 2>&1
+ 
+if [ $? -eq 0 ]
+then
+	echo "- Internet Ok -"	
+else
+	echo "- Internet Failed! -"
+fi
+
+
+# Sync clock
+echo "- Sync Clock -"
+cat >> /etc/chrony.conf <<EOF
+server 0.au.pool.ntp.org
+EOF
+
+systemctl enable --now chronyd.service
+
+echo "- Install Tools -"
+dnf -yqe 3 install net-tools bind-utils wget tree
+
+echo "---- CentOS Base Setup Complete ----"; echo ""
